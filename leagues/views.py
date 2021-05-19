ebe61a5f8993f1f3227234a4ff1ff2971c609876
IgnacioMunoz <imunozr@gmail.com>
2021-05-18 20:13:55 -0400

    asignment1

--- leagues/views.py
+++ leagues/views.py
@@ -8,6 +8,22 @@ def index(request):
 		"leagues": League.objects.all(),
 		"teams": Team.objects.all(),
 		"players": Player.objects.all(),
+		'baseball': League.objects.filter(name__contains='baseball'),
+		'women': League.objects.filter(name__contains='women'),
+		'hockey': League.objects.filter(name__contains='hockey'),
+		'footballNot': League.objects.exclude(name__contains='footbal'),
+		'conference': League.objects.filter(name__contains='conference'),
+		'atlantic': League.objects.filter(name__contains='atlantic'),
+		"dallas": Team.objects.filter(location__contains='dallas'),
+		"raptors": Team.objects.filter(team_name__contains='raptors'),
+		"city": Team.objects.filter(location__contains='city'),
+		"tteams": Team.objects.filter(team_name__startswith='T'),
+		"order": Team.objects.all().order_by('location'),
+		"order2": Team.objects.all().order_by('-team_name'),
+		"edwards": Player.objects.filter(last_name__contains='Edwards'),
+		"joshuas": Player.objects.filter(first_name__contains='Joshua'),
+		"edwards2": Player.objects.filter(last_name__contains='Edwards').exclude(first_name__contains='Joshua'),
+		"alexWyatt": Player.objects.filter(first_name__contains='Alexander') | Player.objects.filter(first_name__contains='Wyatt'),
 	}
 	return render(request, "leagues/index.html", context)
 
